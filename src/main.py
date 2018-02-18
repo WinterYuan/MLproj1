61c3cc16b6422012db12c37df1decde40f8d08d6
winter <yuandongxu869@163.com>
2018-02-18 22:56:15 +0800

    initial

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import kNN
+
+# group,labels = kNN.createDataSet()
+# kNN.classify0([0,0],group,labels,3)
+# datingDataMat,datingLabel = kNN.file2matrix('datingTestSet2.txt')
+# normMat,ranges,minVals = kNN.autoNorm(datingDataMat)
+# print(normMat)
+# print(minVals)
+# kNN.classifyPerson()
+
+# testVector = kNN.img2vector('testDigits/0_13.txt')
+# print(testVector[0,0:31])
+
+kNN.handwritingClassTest()
